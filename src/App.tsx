6189678dfdae5d39c7c2810ae8ba32af82c17585
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-27 16:15:55 +0100

    Create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const App = (): JSX.Element => {
   return <p>Hello Developers!</p>;
 };
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,5 @@
+const Button = (): JSX.Element => {
+  return <button>Sign up</button>;
+};
+
+export default Button;
