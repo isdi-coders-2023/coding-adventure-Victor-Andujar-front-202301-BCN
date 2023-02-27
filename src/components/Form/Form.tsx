182f80b0b80aee8fcb3f266fed9728b587cda270
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-27 17:35:06 +0100

    Modify classNames and change input type for password

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,19 +1,19 @@
 const Form = (): JSX.Element => {
   return (
-    <form className="register-form">
-      <label htmlFor="Email" className="register-form__text">
+    <form className="register">
+      <label htmlFor="Email" className="register__text">
         Email
       </label>
-      <input type="text" className="register-form__input" />
-      <label htmlFor="Password" className="register-form__text">
+      <input type="text" className="register__data" />
+      <label htmlFor="Password" className="register__text">
         Password
       </label>
-      <input type="text" className="register-form__input" />
-      <label htmlFor="Image" className="register-form__text">
+      <input type="password" className="register__data" />
+      <label htmlFor="Image" className="register__text">
         Image
       </label>
-      <input type="file" className="register-form__input" />
-      <button className="register-form__button">Sign up</button>
+      <input type="file" className="register__data" />
+      <button className="register__button">Sign up</button>
     </form>
   );
 };
