84f332aa4520a68ca80ca4b31f8e28a37e712899
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-01 15:26:37 +0100

    Modify className names of Form component

--- src/components/FormRegister/FormRegister.tsx
+++ src/components/FormRegister/FormRegister.tsx
@@ -4,19 +4,15 @@ const FormRegister = (): JSX.Element => {
       <label htmlFor="Email" className="register-form__text">
         Email
       </label>
-      <input type="email" className="register-form__credentials" id="email" />
+      <input type="email" className="register-form__field" id="email" />
       <label htmlFor="Password" className="register-form__text">
         Password
       </label>
-      <input
-        type="password"
-        className="register-form__credentials"
-        id="password"
-      />
+      <input type="password" className="register-form__field" id="password" />
       <label htmlFor="Image" className="register-form__text">
         Image
       </label>
-      <input type="file" className="register-form__credentials" />
+      <input type="file" className="register-form__field" />
       <button className="register-form__button">Sign up</button>
     </form>
   );
