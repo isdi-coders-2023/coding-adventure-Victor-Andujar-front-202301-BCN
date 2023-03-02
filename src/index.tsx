4c17f16500a6b07c8455ac82a8f597cb3407ee0e
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-02 12:47:35 +0100

    Install Robot font and add it at GlobalStyles as secondary font

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyles from "./styles/GlobslStyles";
 import "@fontsource/saira-stencil-one";
+import "@fontsource/roboto";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
